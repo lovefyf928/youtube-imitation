74aee291c0ef9b678e910f1707ce127ae846eebc
xcs1234 <xcsfyf@gmail.com>
2019-04-20 10:30:05 +0800

    login interface

--- youtube-imitation/src/app/login/enter-pwd/enter-pwd.component.ts
+++ youtube-imitation/src/app/login/enter-pwd/enter-pwd.component.ts
@@ -10,14 +10,27 @@ import {Server} from "../../server";
 export class EnterPwdComponent implements OnInit {
   userName: any;
   email: any;
+  password: any;
   constructor(private ar: ActivatedRoute, private s: Server) { }
+
+  login(){
+    this.s.Login(this.userName, this.password).subscribe((res) => {
+      console.log(res)
+      if (res.Data != null) {
+        alert(res.Data.msg);
+        sessionStorage.setItem("token", res.Data.token);
+      }
+      else {
+        alert(res.Msg);
+      }
+    })
+  }
 
   ngOnInit() {
     this.ar.queryParams.subscribe((res) => {
       this.s.SelectUserName(res["account"]).subscribe((res) => {
-        console.log(res);
-        res.Data.email = this.email;
-        res.Data.userName = this.userName;
+        this.email = res.Data.email;
+        this.userName = res.Data.userName;
       })
     })
   }
--- youtube-imitation/src/app/server.ts
+++ youtube-imitation/src/app/server.ts
@@ -8,11 +8,18 @@ export class Server {
 
   constructor(private hc: HttpClient) {
   }
+
+  header = new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded'
+  });
+
   SelectUserName(account) {
-    var body = new HttpParams().set("phoneNumber", account).set("email", account)
-    return this.hc.post(`${this.url}selectusername`, body.toString(), {headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded'
-      })
-    });
+    var body = new HttpParams().set("phoneNumber", account).set("email", account);
+    return this.hc.post(`${this.url}selectusername`, body.toString(), {headers: this.header});
+  }
+
+  Login(userName, password) {
+    var body = new HttpParams().set("userName", userName).set("password", password);
+    return this.hc.post(`${this.url}login`, body.toString(), {headers: this.header});
   }
 }
